a3d0aab2df263becbb7b2091d5769483aa0e163c
Leptos <leptos.0.null@gmail.com>
2021-03-09 20:01:48 -0800

    [OTPassRowController] Restart timer in updateTimingElements

--- nanonetimext/InterfaceControllers/OTPassRowController.m
+++ nanonetimext/InterfaceControllers/OTPassRowController.m
@@ -21,8 +21,6 @@
 - (void)setBag:(OTBag *)bag {
     _bag = bag;
     
-    [_validityTiming invalidate];
-    
     self.issuerLabel.text = bag.issuer;
     self.issuerLabel.accessibilityValue = bag.issuer;
     self.accountLabel.text = bag.account;
@@ -33,14 +31,6 @@
     BOOL isTotp = [bag.generator isKindOfClass:[OTPTime class]];
     self.validityTimer.hidden = !isTotp;
     self.counterButton.hidden = isTotp;
-    
-    if (isTotp) {
-        OTPTime *totp = bag.generator;
-        NSDate *date = [totp nextStepPeriodForDate:[NSDate date]];
-        _validityTiming = [[NSTimer alloc] initWithFireDate:date interval:totp.step target:self
-                                                   selector:@selector(updateTimingElements) userInfo:NULL repeats:YES];
-        [NSRunLoop.mainRunLoop addTimer:_validityTiming forMode:NSDefaultRunLoopMode];
-    }
 }
 
 - (void)updateTimingElements {
@@ -50,7 +40,10 @@
     self.passcodeLabel.text = passcode;
     self.passcodeLabel.accessibilityValue = passcode;
     
+    [_validityTiming invalidate];
+    
     if ([generator isKindOfClass:[OTPTime class]]) {
+        NSDate *fireDate = [generator nextStepPeriodForDate:[NSDate date]];
         // Note: WatchKit accessibility elements aren't as smart as their UIKit counterparts
         //   if you set the accessibilityLabel on a UILabel, the text property will be used for the accessibilityValue
         //   doing the same on a WKInterfaceLabel, the accessibilityValue is nil unless manually set
@@ -58,8 +51,12 @@
         //   for this reason, I'm not setting the accessibilityLabel on validityTimer,
         //     and the readable countdown is provided by the system, reflecting the time on the screen
         //   the result of this is that a screen reader will only say "11 seconds" instead of "valid for 11 seconds"
-        self.validityTimer.date = [generator nextStepPeriodForDate:[NSDate date]];
+        self.validityTimer.date = fireDate;
         [self.validityTimer start];
+        
+        NSTimer *validityTiming = [[NSTimer alloc] initWithFireDate:fireDate interval:0 target:self selector:_cmd userInfo:nil repeats:NO];
+        [NSRunLoop.mainRunLoop addTimer:validityTiming forMode:NSDefaultRunLoopMode];
+        _validityTiming = validityTiming;
     }
 }
 
